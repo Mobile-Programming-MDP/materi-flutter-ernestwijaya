627cce12e0636a909b1b07559f7212cb0be638ed
ernestwijaya <ernestwijaya_2428250010@mhs.mdp.ac.id>
2026-05-15 20:26:58 +0700

    notification

--- cepu_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ cepu_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,10 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_database
+import firebase_messaging
 import firebase_storage
 import flutter_image_compress_macos
+import flutter_local_notifications
 import geolocator_apple
 import package_info_plus
 import share_plus
@@ -23,8 +25,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
